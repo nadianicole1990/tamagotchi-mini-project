687a2d32a2aa9e40c1faebc9076f6ca82f530a88
Nadia Gushin <nadiagushin@Nadias-MacBook-Pro.local>
2024-04-23 19:08:49 -0400

    Files linked && Initial pseudocode

---
+++ js/script.js
@@ -0,0 +1,23 @@
+alert("Your files are totally linked!")
+
+// Create an Object or Class for your Tamagotchi
+
+// Instantiate your Tamagotchi
+
+// Add the ability to name your pet.
+// Increase your pet's age every x minutes
+
+// Increase your pet's Hunger, Sleepiness, and Bored metrics on an interval of your choosing.
+
+// You pet should die if Hunger, Boredom, or Sleepiness hits 10.
+// Alert “Your pet has crossed the rainbow bridge”
+
+// Morph your pet at certain ages.
+
+// Animate your pet across the screen while it's alive.
+
+
+// EXTRAS
+// Have your Tamagotchi give birth to baby Tamagotchi...
+// ...with special powers (extend the class)!
+// Add an exercise() method to your Tamagotchi, that affects certain properties
