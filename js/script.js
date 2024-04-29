53e496bcdb2c4b1851d084d261d8e3a164c21a71
Nadia Gushin <nadiagushin@Nadias-MacBook-Pro.local>
2024-04-28 23:30:07 -0400

    Initial code for exercise && Improved rainbowBridge && Light/Dark modes created

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ class Nadagotchi {
         this.hunger = 0;
         this.sleepiness = 0;
         this.boredom = 0;
+        this.chonkiness = 0
     }
     growUp() {
             this.age++
@@ -30,12 +31,33 @@ class Nadagotchi {
             return this.boredom;
         }
     }
+    getChonky() {
+        if (this.chonkiness < 10) {
+            this.chonkiness++;
+            return this.chonkiness;
+        }
+    }
 }
 // Instantiate your Tamagotchi
 const blackKitty = new Nadagotchi("Grump");
 // Add the ability to name your pet.
 const nameOfPet = prompt("Name your pet!");
 nametarget.innerHTML = "Hello, My Name is " + nameOfPet + "!"
+// Change egg style
+let lightEgg = document.getElementById("egg");
+let darkEgg = document.getElementById("altegg");
+const lightButton = document.querySelector("#light");
+lightButton.addEventListener("click", lightMode);
+function lightMode() {
+        lightEgg.style.visibility = "visible";
+        darkEgg.style.visibility = "hidden";
+}
+const darkButton = document.querySelector("#dark");
+darkButton.addEventListener("click", darkMode);
+function darkMode() {
+    lightEgg.style.visibility = "hidden";
+    darkEgg.style.visibility = "visible";
+}
 // Display metrics for pet: setInterval updates HTML for metrics every second
 setInterval(function(){
     const ageLog = document.getElementById("agetarget");
@@ -46,17 +68,20 @@ setInterval(function(){
     sleeptarget.innerHTML = blackKitty.sleepiness;
     const boredLog = document.getElementById("boredtarget");
     boredtarget.innerHTML = blackKitty.boredom;
+    const chonkLog = document.getElementById("chonktarget");
+    chonktarget.innerHTML = blackKitty.chonkiness;
 }, 1000)
 // Increase your pet's age every x minutes
 agingInterval = setInterval(function() {
     blackKitty.growUp();
-}, 4000)
+}, 8000)
 // Increase your pet's Hunger, Sleepiness, and Bored metrics on an interval of your choosing.
 metricInterval = setInterval(function() {
     blackKitty.getHungry();
     blackKitty.getSleepy();
     blackKitty.getBored();
-}, 2000)
+    blackKitty.getChonky();
+}, 1000)
 const feedButton = document.querySelector("#feed");
 feedButton.addEventListener("click", feed);
 function feed() {
@@ -75,33 +100,38 @@ function play() {
     blackKitty.boredom-=1;
     return blackKitty.boredom;
 }
+const exerciseButton = document.querySelector("#exercise");
+exerciseButton.addEventListener("click", exercise); 
+function exercise() {
+    blackKitty.chonkiness-=1;
+    return blackKitty.chonkiness;
+}
+let babyPic = document.getElementById("kitty");
+let bigBoyPic = document.getElementById("cat");
 // Morph your pet at certain ages.
 petMorphs = setInterval(function() {
     morph();
 }, 1000);
 function morph() {
-    if (blackKitty.age >= 5) {
-        let babyPic = document.getElementById("kitty");
-        let bigBoyPic = document.getElementById("cat");
+    if (blackKitty.age > 5) {
         babyPic.style.visibility = "hidden";
         bigBoyPic.style.visibility = "visible";
-        // babyPic.src = "./images/gentlecat.png"
-        // babyPic.setAttribute("src", "./images/gentlecat.png");
-
     }
 }
+// Your pet should die if Hunger, Boredom, or Sleepiness hits 10.
 petDies = setInterval(function() {
     rainbowBridge();
-}, 1000);
+}, 500);
 function rainbowBridge() {
-    if (blackKitty.hunger === 10 || blackKitty.sleepiness === 10 || blackKitty.boredom === 10) {
+    if (blackKitty.hunger === 10 || blackKitty.sleepiness === 10 || blackKitty.boredom === 10 || blackKitty.chonkiness === 10) {
         clearInterval(metricInterval);
         clearInterval(agingInterval);
         clearInterval(petDies);
+        babyPic.style.visibility = "hidden";
+        bigBoyPic.style.visibility = "hidden";
         alert(nameOfPet + " has crossed the rainbow bridge :(");
     }
 }
 // EXTRAS
 // Have your Tamagotchi give birth to baby Tamagotchi...
 // ...with special powers (extend the class)!
-// Add an exercise() method to your Tamagotchi, that affects certain properties
